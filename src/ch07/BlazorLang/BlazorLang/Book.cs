20119824277939b0504e4424f006a3f221b8f99b
Tomoaki Masuda <masuda@moonmile.net>
2024-12-26 01:47:36 +0900

    ch08 まで

--- src/ch07/BlazorLang/BlazorLang/Book.cs
+++ src/ch07/BlazorLang/BlazorLang/Book.cs
@@ -8,9 +8,9 @@ namespace BlazorLang
 {
     public class Book
     {
-        public string Title { get; set; }
-        public string Author { get; set; }
+        public string Title { get; set; } = "";
+        public string Author { get; set; } = "";
         public int Price { get; set; }
-        public string Publisher { get; set; }
+        public string Publisher { get; set; } = "";
     }
 }
